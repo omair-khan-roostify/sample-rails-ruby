177b16e6cd1d5cffb370ae97f7e32621a6ac527f
noboru-i <ishikura.noboru@gmail.com>
2018-06-12 14:57:24 +0900

    add sample

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,32 @@ class Article < ApplicationRecord
   def title?
     title.present?
   end
+
+  # Test Rails Best Practices warning (remove unused methods)
+  def unused_method
+    has_title
+    too_many
+  end
+
+  # Test Rubocop warning (Naming/PredicateName)
+  def has_title
+    true
+  end
+
+  # Test reek warning (TooManyStatements)
+  def too_many
+    sample_method
+    sample_method
+    sample_method
+    sample_method
+    sample_method
+    sample_method
+    sample_method
+    sample_method
+    sample_method
+    sample_method
+    sample_method
+    sample_method
+    sample_method
+  end
 end
